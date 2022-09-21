bac7a652dca5e2d2839daee35715b2c032421df9
Bilalov Danis <hytagyn@gmail.com>
2022-09-21 21:37:12 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket= 100;
+        int bonus= ticket/20;
+
+        System.out.println ((bonus) + " Бонусные мили");
+    }
+}
